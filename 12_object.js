979ad69a72487b0ccc6f03c9cc2533aa04911ae3
keertivanth chaudhary <keertivanth0@gmail.com>
2026-02-05 07:54:48 +0530

    object

---
+++ 12_object.js
@@ -0,0 +1,20 @@
+//object literals
+const sym = Symbol("key");
+const user={
+    name:"kv",//form og key-value
+    age:21,
+    [sym]:"mykey",//to consider as a symbol adding from above 
+    location: "meerut",
+    email :"kv@gmail.com"
+}
+// console.log(user.email)
+// console.log(user["email"]);
+// console.log(user[sym]);
+
+//user.email="kv2@gmail.com";
+//Object.freeze(user);//revent from changes in object
+//console.log(user);
+ user.greeting=function(){
+    console.log("kv");
+ }
+ console.log(user.greeting());
