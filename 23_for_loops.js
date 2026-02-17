8c5773f96f26417556d123c3f31a5c6d52e97a77
keertivanth chaudhary <keertivanth0@gmail.com>
2026-02-17 06:33:39 +0530

    loops

---
+++ 23_for_loops.js
@@ -0,0 +1,29 @@
+for(let i=0;i<10;i++){
+    const element=i;
+   // console.log(element);
+
+}
+let a = ["swift","scorpio","virtus"]
+console.log(a.length);
+for(let i=0;i<a.length;i++){
+    const element=a[i];
+   // console.log(element);
+
+}
+
+// while loops
+let i=0;
+while(i<=10){
+    console.log(`valuse of index is ${i}`);
+    i=i+2;
+}
+let arr=0
+while(arr<a.length){
+    console.log(`value is ${a[arr]}`);
+    arr=arr+1;
+}
+let score=1;
+do{
+    console.log(`score is ${score}`);
+    score++;
+}while(score<=10);
